325e706238a68a4c32cce474e13a691560e7597a
ulfatunnisareal <tunnisaulfa67@gmail.com>
2023-10-30 14:44:40 +0700

    Mencoba tugas

---
+++ database/migrations/2023_10_27_131505_create_students_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('students', function (Blueprint $table) {
+            $table->id();
+            $table->char('nim', 8); 
+            $table->string('nama', 30); 
+            $table->string('prodi', 30); 
+            // untuk field created_at dan updated_at 
+            $table->timestamps(); });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('students');
+    }
+};
---
+++ routes/web.php
@@ -0,0 +1,30 @@
+<?php
+
+use App\Http\Controllers\StudentController;
+use App\Http\Controllers\LecturerController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/students', [StudentController::class, 'index']);
+Route::get('/students/tambah', [StudentController::class, 'tambah']); 
+Route::post('/students/simpan', [StudentController::class, 'simpan']); 
+Route::get('/students/tampil/{id}', [StudentController::class, 'tampil']); 
+Route::get('/students/ubah/{id}', [StudentController::class, 'ubah']); 
+Route::put('/students/perbarui/{id}', [StudentController::class, 'perbarui']); 
+Route::get('/students/hapus/{id}', [StudentController::class, 'hapus']);
+
+Route::resource('/lecturers', LecturerController::class);
